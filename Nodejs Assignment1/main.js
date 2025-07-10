bc2eab38e00d5f6526fe29b20ba714ddd16d4ef6
Harsha1621 <96712747+Harsha1621@users.noreply.github.com>
2025-07-10 04:34:23 +0000

    Assignment1

---
+++ Nodejs Assignment1/main.js
@@ -0,0 +1,5 @@
+// Import with aliases
+import { add as sum, subtract as diff } from './mathOperations.js';
+
+console.log('Sum:', sum(10, 5));        // Output: Sum: 15
+console.log('Difference:', diff(10, 5)); // Output: Difference: 5
