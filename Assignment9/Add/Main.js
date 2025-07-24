c0df999fb5cfcdef16ce6eac9216bd29d6670682
Harsha1621 <96712747+Harsha1621@users.noreply.github.com>
2025-07-24 09:36:39 +0530

    Add files via upload

---
+++ Assignment9/Add/Main.js
@@ -0,0 +1,3 @@
+import { add as sum, subtract as diff } from './mathOperations.js';
+console.log("Sum of 5 and 3:", sum(5, 3));
+console.log("Difference of 10 and 4:", diff(10, 4));
---
+++ Assignment9/Add/mathOperations.js
@@ -0,0 +1,10 @@
+export function add(a, b) {
+return a + b;
+}
+export function subtract(a, b) {
+return a - b;
+}
+Main.js
+import { add as sum, subtract as diff } from './mathOperations.js';
+console.log("Sum of 5 and 3:", sum(5, 3));
+console.log("Difference of 10 and 4:", diff(10, 4));
